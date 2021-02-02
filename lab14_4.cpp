d4782df9a6c0dcf3de578120acbc90eb17aae210
kittiphum <kittiphum_s@cmu.ac.th>
2021-02-02 17:10:52 +0700

    เสร็จ

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -17,3 +17,21 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int x[] = {1000,500,100,50};
+	int r,temp;
+	int y[4];
+	for(int i = 0;i < 4;i++){
+		r = rand()%(4-i);
+		y[i] = x[r];
+		temp = x[3-i];
+		x[3-i] = x[r];
+		x[r] = temp;
+	}
+	a = y[0];
+	b = y[1];
+	c = y[2];
+	d = y[3];
+}
+
